23a669ac2482bc025c37f0d26ca3ce239bca3e3a
riccardo fiorani <64063131+fiorani@users.noreply.github.com>
2022-06-22 21:05:30 +0200

    first version with file transfer

--- client.py
+++ client.py
@@ -12,35 +12,29 @@ if not os.path.exists(os.path.join(os.getcwd(), "file_client")):
    os.mkdir(os.path.join(os.getcwd(), "file_client"))
    
 path = os.path.join(os.getcwd(), 'file_client')
-file=os.listdir(path)
-print(file)
+print(os.listdir(path))
 
 sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 port=10000;
 server_address = ('localhost', port)
-message = 'bella'
+message = 'client.png'
+buffer=2048
 
 try:
-
-    # inviate il messaggio
-    print ('sending "%s"' % message)
-    time.sleep(2) #attende 2 secondi prima di inviare la richiesta
     packet=message.encode()
-    checksum = checksum_calculator(packet)
-    data_length = len(packet)
-    udp_header = struct.pack("!IIII", port, port, data_length, checksum)
-    #test corrotto
-    message = 'bel'
-    packet=message.encode()
-    packet_with_header = udp_header + packet
-    sent = sock.sendto(packet_with_header, server_address)
+    udp_header = struct.pack("!IIII", port, port, len(packet), checksum_calculator(packet))
+    sent = sock.sendto(udp_header + packet, server_address)
+    file = open(message, "rb") 
+    chunk= file.read(buffer)
+    while chunk:   
+        packet=chunk
+        udp_header = struct.pack("!IIII", port, port, len(packet), checksum_calculator(packet))
+        sent = sock.sendto(udp_header + packet, server_address)
+        chunk = file.read(buffer)
+        
+    file.close
 
-    # Ricevete la risposta dal server
-    print('waiting to receive from')
-    data, server = sock.recvfrom(4096)
-    #print(server)
-    time.sleep(2)
-    print ('received message "%s"' % data.decode('utf8'))
+        
 except Exception as info:
     print(info)
 finally:
--- server.py
+++ server.py
@@ -19,38 +19,46 @@ file=os.listdir(path)
 print(file)
 
 sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
-server_address = ('localhost', 10000)
+port=10000;
+server_address = ('localhost', port)
 sock.bind(server_address)
 print ('\n\r starting up on %s port %s' % server_address)
-
+buffer=4096
 
 
 while True:
-    print('\n\r waiting to receive message...')
-    
-    data_rcv, address = sock.recvfrom(4096)
+    print('aspetto')
+    data_rcv, address = sock.recvfrom(buffer)
     udp_header = data_rcv[:16]
     data = data_rcv[16:]
     udp_header = struct.unpack("!IIII", udp_header)
     correct_checksum = udp_header[3]
     checksum = checksum_calculator(data)
-
-    print('received %s bytes from %s' % (len(data), address))
     print (data.decode('utf8'))
     
     if correct_checksum != checksum:
-        data1='corrotto'
-        time.sleep(2)
-        sent = sock.sendto(data1.encode(), address)
-        print ('sent %s bytes back to %s' % (sent, address))
+        print('arrivato corrotto')
     elif data:
-        data1='arrivato'
-        time.sleep(2)
-        sent = sock.sendto(data1.encode(), address)
-        print ('sent %s bytes back to %s' % (sent, address))
+        print('arrivato')
+        data_rcv, address = sock.recvfrom(buffer)
+        udp_header = data_rcv[:16]
+        data = data_rcv[16:]
+        udp_header = struct.unpack("!IIII", udp_header)
+        correct_checksum = udp_header[3]
+        checksum = checksum_calculator(data)
+        file= open("server.png", "wb") 
+        chunk = data
+        while chunk:   
+            file.write(chunk)
+            data_rcv, address = sock.recvfrom(buffer)
+            data = data_rcv[16:]
+            chunk = data
+            
+            
+        file.close
     else:
-        data1='arrivato vuoto'
-        time.sleep(2)
-        sent = sock.sendto(data1.encode(), address)
-        print ('sent %s bytes back to %s' % (sent, address))
+        print('arrivato vuoto')
+
+
+
 
