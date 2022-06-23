6fdaf48e41ecd4517ac130dd647911f6491ba309
riccardo fiorani <64063131+fiorani@users.noreply.github.com>
2022-06-23 12:06:19 +0200

    fixed file close

--- client.py
+++ client.py
@@ -19,7 +19,7 @@ sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 port=10000;
 server_address = ('localhost', port)
 file = 'client.png'
-buffer=1024
+buffer=4096*2
 
 try:
     
@@ -29,23 +29,22 @@ try:
     udp_header = struct.pack("!IIII", 1, port, len(packet), checksum_calculator(packet))
     sent = sock.sendto(udp_header + packet, server_address)
     
-    tot_packs = math.ceil(os.path.getsize(file)/buffer)
+    tot_packs = math.ceil(os.path.getsize(file)/buffer)+1
     count=0
     file = open(file, "rb") 
-    chunk= file.read(buffer)
-    while chunk:   
+    
+    while True:
+        chunk= file.read(buffer)
         packet=chunk
-        udp_header = struct.pack("!IIII", 2, port, len(packet), checksum_calculator(packet))
+        udp_header = struct.pack("!IIII", 2, count, len(packet), checksum_calculator(packet))
         sent = sock.sendto(udp_header + packet, server_address)
-        chunk = file.read(buffer)
         count+=1
+        if count==tot_packs:
+            print("inviato ",count," su ",tot_packs)
+            break
         
-    file.close
-    
-    print("inviato ",count," su ",tot_packs)
-    if count==tot_packs:
-        print("inviati tutti")
-    
+    file.close()
+   
     message = 'inviato'
     packet=message.encode()
     udp_header = struct.pack("!IIII", 3, tot_packs, len(packet), checksum_calculator(packet))
--- server.py
+++ server.py
@@ -23,7 +23,7 @@ port=10000;
 server_address = ('localhost', port)
 sock.bind(server_address)
 print ('\n\r starting up on %s port %s' % server_address)
-buffer=4096
+buffer=4096*4
 
 
 while True:
@@ -41,19 +41,9 @@ while True:
     elif data:
         print('connessione stabilità')
         count=0
-        data_rcv, address = sock.recvfrom(buffer)
-        udp_header = data_rcv[:16]
-        data = data_rcv[16:]
-        udp_header = struct.unpack("!IIII", udp_header)
-        correct_checksum = udp_header[3]
-        checksum = checksum_calculator(data)
-        if correct_checksum != checksum:
-            print("corrotto")
         file= open("server.png", "wb") 
-        chunk = data
-        while chunk:   
-            file.write(chunk)
-            count+=1
+        while True:   
+            
             data_rcv, address = sock.recvfrom(buffer)
             udp_header = data_rcv[:16]
             data = data_rcv[16:]
@@ -65,12 +55,16 @@ while True:
                 print("corrotto")
             if a==3:
                 print ("arrivati ",count," su ",b)
-                if count==b:
-                    print("arrivati tutti")
                 break
-            chunk = data
             
-        file.close
+            print (b)
+            if b!=count:
+                print("error")
+            chunk = data
+            file.write(chunk)
+            count+=1
+            
+        file.close()
         
     else:
         print('arrivato vuoto')
