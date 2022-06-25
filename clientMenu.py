b1e803ef1c988dd0dae3bf459018f4a6a557c46a
Mattia Panni <mattia.panni@studio.unibo.it>
2022-06-25 15:44:13 +0200

    Improvements

--- clientMenu.py
+++ clientMenu.py
@@ -23,8 +23,7 @@ class ui:
         self.lbl.pack()
         self.operations = ("Get files stored on server", "Download file from the server", "Upload file onto the server", "Close connection with the server")
         self.msg_list = tkt.Listbox(self.messages_frame, heigh = 15, width = 50, yscrollcommand = self.scrollbar.set, selectmode = "browse")
-        for i in range(0, len(self.operations)):
-            self.msg_list.insert(i, self.operations[i]) 
+        self.set_msg_list(self.operations)
         self.scrollbar.pack(side = tkt.RIGHT, fill = tkt.Y)
         self.msg_list.pack(side = tkt.LEFT, fill = tkt.BOTH)
         self.msg_list.pack()
@@ -35,9 +34,18 @@ class ui:
         self.send_button.pack()
         tkt.mainloop()
 
+    def clear_msg_list(self):
+        self.msg_list.delete(0, tkt.END)
+        
+    def set_msg_list(self, list):
+        for i in range(0, len(list)):
+            self.msg_list.insert(i, list[i]) 
+
     def choose_operation(self, client):
         if self.msg_list.get(self.msg_list.curselection()) == self.operations[0]:
-            self.msg_list.insert(0, client.get_files_from_server())
+            self.clear_msg_list()
+            files = list(client.get_files_from_server().split("\n"))            
+            self.set_msg_list(files)
         elif self.msg_list.get(self.msg_list.curselection()) == self.operations[1]:
             print("2")
         elif self.msg_list.get(self.msg_list.curselection()) == self.operations[2]:
