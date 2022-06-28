a472771e08ae2d925a31b589192175364ffa842e
riccardo fiorani <64063131+fiorani@users.noreply.github.com>
2022-06-28 12:21:42 +0200

    function fix

--- utilities.py
+++ utilities.py
@@ -5,8 +5,9 @@ def checksum_calculator(data):
  checksum = zlib.crc32(data)
  return checksum
 
-def return_list_of_files_in(directoryName):
+def create_directory(directoryName):
     if not os.path.exists(os.path.join(os.getcwd(), directoryName)):
        os.mkdir(os.path.join(os.getcwd(), directoryName))     
-    path = os.path.join(os.getcwd(), directoryName)
-    return os.listdir(path)
+
+def return_list_of_files_in(directoryName):
+    return os.listdir(os.path.join(os.getcwd(), directoryName))
