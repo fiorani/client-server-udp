57f6bbb257a0b23b4e31299123894856c8707f82
Mattia Panni <mattia.panni@studio.unibo.it>
2022-06-29 18:45:13 +0200

    Add segment factory

---
+++ segmentFactory.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 29 17:15:35 2022
+
+@author: pnmat
+"""
+
+import struct 
+from operationType import OperationType as OPType
+import utilities as ut
+
+class SegmentFactory:
+    
+    def getBeginConnectionSegment(port):
+        return struct.pack("!IIII", OPType.BEGIN_CONNECTION.value, port, 0, 0)
+    
+    def getTotalFramesSegment(tot_frames):
+        return struct.pack("!IIII", OPType.BEGIN_CONNECTION.value, 0, tot_frames, 0)
+    
+    def getUploadFrameSegment(seqNumber, frame):
+        return struct.pack("!IIII", OPType.UPLOAD.value, 0, seqNumber, ut.checksum_calculator(frame.encode())) + frame.encode()
+    
+    def getDownloadRequestSegment(filename):
+        return struct.pack("!IIII", OPType.DOWNLOAD.value, 0, 0, ut.checksum_calculator(filename.encode())) + filename.encode()
+        
+    def getACKSegment(seqNumber):
+        return struct.pack("!IIII", OPType.ACK.value, 0, seqNumber, 0)
+    
+    def getNACKSegment(seqNumber):
+        return struct.pack("!IIII", OPType.NACK.value, 0, seqNumber, 0)
+    
+    def getCloseConnectionSegment():
+        return struct.pack("!IIII", OPType.CLOSE_CONNECTION.value, 0, 0, 0)
+    
